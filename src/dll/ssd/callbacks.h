63f6e0969773c1586b10c062d832c42431220b2a
neru <152752583+neeeruuu@users.noreply.github.com>
2024-07-16 21:16:15 -0300

    feat: add imgui callback

--- src/dll/ssd/callbacks.h
+++ src/dll/ssd/callbacks.h
@@ -9,5 +9,7 @@ struct _D3DPRESENT_PARAMETERS_;
 inline auto g_D3DPresentCallback = new CallbackEvent<IDirect3DDevice9*>();
 inline auto g_D3DResetCallback = new CallbackEvent<IDirect3DDevice9*, _D3DPRESENT_PARAMETERS_*>();
 
+inline auto g_ImGuiCallback = new CallbackEvent();
+
 inline auto g_PostEnginePostProcessingCallback = new CallbackEvent();
 inline auto g_PreRenderViewCallback = new CallbackEvent<CViewSetup&>();
